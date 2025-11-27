0b5ff5a3bc9cf09ecbb92e48baf7426990ffba8d
LEEJUNWOO <junu5810@naver.com>
2025-11-27 16:55:02 +0900

    turtlesim_node 액션 추가버전

--- subscriber.py
+++ subscriber.py
@@ -5,7 +5,6 @@ import numpy as np
 def callback(message):
     ranges = np.array(message['ranges'])
 
-    # LDS-02는 angle_increment = 1도 라고 가정
     front = np.r_[ranges[350:360], ranges[0:10]]
     left  = ranges[80:100]
     right = ranges[260:280]
@@ -23,9 +22,17 @@ def callback(message):
 
     print(f"front={front_dist:.2f}, left={left_dist:.2f}, right={right_dist:.2f} -> action={action}")
 
+    action_msg = roslibpy.Message({'data': action})
+    publisher.publish(action_msg)
+
+
 def main():
     client = roslibpy.Ros(host='172.19.160.132', port=9090)
     client.run()
+
+    global publisher
+    publisher = roslibpy.Topic(client, '/turtle_action', 'std_msgs/String')
+    publisher.advertise()
 
     subscriber = roslibpy.Topic(client, '/scan', 'sensor_msgs/LaserScan')
     subscriber.subscribe(callback)
@@ -35,6 +42,7 @@ def main():
             pass
     except KeyboardInterrupt:
         subscriber.unsubscribe()
+        publisher.unadvertise()
         client.terminate()
 
 if __name__ == '__main__':
